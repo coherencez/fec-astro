a8f6530269d03eec859d74b116ab0354c3a9a3e0
Thomas Swann <tkswann@mac.com>
2016-07-29 13:22:13 -0500

    default view WIP

--- app/controllers/defaultCtrl.js
+++ app/controllers/defaultCtrl.js
@@ -1,5 +1,22 @@
 'use strict';
 
-app.controller('defaultCtrl', ['$scope', function($scope) {
+app.controller('defaultCtrl', ['$scope', '$route', 'dataFactory', function($scope, $route, df) {
+  $scope.arr = [];
 
+  $scope.makeArray = (data) => {
+    angular.forEach(data, (v, k) => {
+      $scope.arr.push(v);
+    });
+    $scope.rndPicture();
+  };
+
+  $scope.rndPicture = () => {
+    let rndIndex = df.rndNum(0, $scope.arr.length - 1);
+    $scope.splashPic = $scope.arr[rndIndex];
+    console.log($scope.splashPic);
+  };
+
+
+
+  df.getPictures($scope.makeArray);
 }]);
