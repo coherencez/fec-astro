13d969cfb9822614a8ca7ba72209c2c918be3d1f
Thomas Swann <tkswann@mac.com>
2016-07-31 20:24:43 -0500

    added iife for description dropdown

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -1,5 +1,10 @@
 'use strict';
 
 app.controller('profileCtrl', ['$scope', function($scope) {
+  $scope.initiate = (function () {
+    $('.expander-trigger').click(function(){
+      $(this).toggleClass("expander-hidden");
+      });
+   })();
 
 }]);
