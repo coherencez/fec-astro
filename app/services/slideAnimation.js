40df452984697a26c750c1cd5709c0a68ce691f9
Thomas Swann <tkswann@mac.com>
2016-08-01 21:21:30 -0500

    added animation WIP

---
+++ app/services/slideAnimation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// app.animation('.slide', [function() {
+//   return {
+//     // make note that other events (like addClass/removeClass)
+//     // have different function input parameters
+//     enter: function(element, doneFn) {
+//       $(element).slideIn(1000, doneFn);
+//
+//       // remember to call doneFn so that angular
+//       // knows that the animation has concluded
+//     },
+//
+//     move: function(element, doneFn) {
+//       $(element).fadeIn(1000, doneFn);
+//     },
+//
+//     leave: function(element, doneFn) {
+//       $(element).slideOut(1000, doneFn);
+//     }
+//   }
+// }]);
