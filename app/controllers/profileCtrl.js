4e88a8832d664ce77c731e955816df24f54efe02
Thomas Swann <tkswann@mac.com>
2016-08-02 20:57:27 -0500

    organized sass folder

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -2,16 +2,7 @@
 
 app.controller('profileCtrl', ['$scope','profileFactory', 'authFactory', '$window', function($scope, proFac, af, $window) {
   $scope.userId = af.getUser();
-  // $scope.initiate = (function () {
-  //   $('.expander-trigger').click(function(e) {
-  //     // grab the img & button group for the clicked favorite list item  only
-  //     let $favImg = $(e.currentTarget.parentNode.parentNode.parentNode.children[0]),
-  //         $buttonGroup = $(e.currentTarget.parentNode.parentNode.parentNode.children[2]);
-  //     $(this).toggleClass('expander-hidden');
-  //     $favImg.toggleClass('hidden');
-  //     $buttonGroup.toggleClass('hidden');
-  //   });
-  // })();
+  
   $scope.dropDown = function (e) {
       let $favImg = $(e.currentTarget.parentNode.parentNode.parentNode.children[0]),
           $buttonGroup = $(e.currentTarget.parentNode.parentNode.parentNode.children[2]);
@@ -37,7 +28,7 @@ app.controller('profileCtrl', ['$scope','profileFactory', 'authFactory', '$windo
 
   $scope.deleteFavorite = (favId) => {
     proFac.deleteFromFavorites(favId);
-    console.log('delted song:', favId);
+    console.log('deleted img:', favId);
   };
 
 
